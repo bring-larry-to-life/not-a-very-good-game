ccb2b7f4ff643c13d3905e1df4dfeaa887addc32
jasonkylemorse <jasonkylemorse@gmail.com>
2017-02-12 16:21:57 -0500

    square changes color on click

---
+++ Assets/SquareBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareBehavior : MonoBehaviour {
+
+	void Start() {
+		print("GAME STARTED");
+	}
+
+	void Update() {
+
+	}
+
+	void OnMouseDown() {
+		print("COMPONENT CLICKED");
+		gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+ 	}
+
+}
