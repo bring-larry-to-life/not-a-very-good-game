18577b7400826ebc8c0b2f63120f7d72ba9fbd05
jasonkylemorse <jasonkylemorse@gmail.com>
2017-03-05 13:38:32 -0500

    title and circle

---
+++ Assets/CircleBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CircleBehavior : MonoBehaviour {
+
+	void Start() {
+		print("GAME STARTED");
+	}
+
+	void Update() {
+
+	}
+
+	void OnMouseDown() {
+		print("COMPONENT CLICKED");
+		SceneManager.LoadScene("circle");
+		//gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+	}
+
+}
---
+++ Assets/Text.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour {
+
+	public Text myText;
+
+	// Use this for initialization
+	void Start () {
+		//myText.text = "Ta-dah!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
